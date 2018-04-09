d85a9ef5a1e8f119b3971d1c1aea446a951fa65f
a17pongo <cEh7Yuf4>
2018-04-09 11:29:22 +0200

    Added new view for mountain details added card to mountain details

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.brom.activitiesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
@@ -13,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> listdata = new ArrayList<String>(Arrays.asList(mountainNames));
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.my_text,listdata);
+
+        ListView listView = (ListView) findViewById(R.id.my_list);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String toast = "Name: " + mountainNames[position] + '\n' + "Location: " + mountainLocations[position] + '\n' + "Height: " + mountainHeights[position];
+                Toast.makeText(MainActivity.this,toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setAdapter(adapter);
 
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
